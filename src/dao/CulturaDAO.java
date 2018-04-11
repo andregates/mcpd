7db7445ae54801fffd2595506e1b02560ebf484b
gabriel <j.quaresmasantos_98@hotmail.com>
2018-04-11 09:22:16 -0300

    Graficos v1

--- src/dao/CulturaDAO.java
+++ src/dao/CulturaDAO.java
@@ -1,7 +1,45 @@
 package dao;
 
-import models.Cultura;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
-public class CulturaDAO extends GenericDAO<Cultura>{
+import org.hibernate.Session;
+import org.hibernate.query.Query;
+
+import models.Cultura;
+import util.HibernateUtil;
+
+public class CulturaDAO extends GenericDAO<Cultura> {
+
+	public Set<Cultura> findByCulturaRegistro() {
+		try {
+			Session sessao = HibernateUtil.getSessionFactory().openSession();
+			Set<Cultura> result = new HashSet<Cultura>();
+			Query query = sessao.getSession().createQuery(
+					"select r.cultura.culturaId, r.cultura.nome from Registro r ");
+
+			@SuppressWarnings("unchecked")
+			List<Object> rs = query.list();
+			for (int i = 0; i < rs.size(); i++) {
+
+				Object[] obj = (Object[]) rs.get(i);
+
+				Cultura cultura = new Cultura();
+				cultura.setCulturaId((Integer) obj[0]);
+				cultura.setNome(((String) obj[1]));
+
+				result.add(cultura);
+
+			}
+
+			return result;
+		} catch (Exception e) {
+			throw e;
+
+		}
+
+	}
 
 }
--- src/dao/RegistroDAO.java
+++ src/dao/RegistroDAO.java
@@ -1,69 +1,87 @@
 package dao;
 
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Date;
-import java.util.Iterator;
 import java.util.List;
 
-import javax.sql.DataSource;
-
-import org.hibernate.query.Query;
-import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.RowMapper;
 import org.hibernate.Session;
+import org.hibernate.query.Query;
+
 import models.Cultura;
 import models.Registro;
 import util.HibernateUtil;
 
 public class RegistroDAO extends GenericDAO<Registro> {
 
+	
+	/**Consulta que retorna lista projetada para geração de grafico. 
+	 * Implementação baseada em consultas realizadas no sistema SIPAC, mantido pela Superintendencia de Informatica da UFRN (SINFO)*/
 	@SuppressWarnings("unchecked")
 	public List<List<Registro>> gerarGraficoProjetado(Date dateInicial, Date dateFinal,
 			List<Cultura> culturasSelecionadas) {
+		Session sessao = HibernateUtil.getSessionFactory().openSession();
+		try {
+			List<List<Registro>> registroList = new ArrayList<List<Registro>>();
+			List<Registro> result = new ArrayList<Registro>();
 
-		List<Registro> registros = new ArrayList<>();
-		List<List<Registro>> registrosAll = new ArrayList<>(); /*
-																 * Session session =
-																 * HibernateUtil.getSessionFactory().openSession();
-																 * 
-																 * for (int i = 0; i < culturasSelecionadas.size(); i++)
-																 * { String hql =
-																 * "select c.culturaId, r.escala, c.nome, r.dataRegistro from Registro r "
-																 * + " inner join r.cultura as c" +
-																 * "	where c.culturaId =:id";
-																 * 
-																 * 
-																 * Query query = session.createQuery(hql);
-																 * query.setParameter("id",
-																 * culturasSelecionadas.get(i).getCulturaId());
-																 * 
-																 * registros = query.list();
-																 * 
-																 * registrosAll.add(registros); }
-																 */
+			for (int i = 0; i < culturasSelecionadas.size(); i++) {
 
-		for (int i = 0; i < culturasSelecionadas.size(); i++) {
-			registros = (List<Registro>) getJdbcTemplate().query(
-					"select cult.culturaId, regist.escala, cult.nome, regist.dataRegistro from registros as regist inner join culturas as cult on (cult.culturaId = regist.culturaId)"
-							+ " WHERE cult.culturaId = ?",
-					new Object[] { culturasSelecionadas.get(i).getCulturaId() }, new RowMapper() {
+				Query query = sessao.getSession()
+						.createQuery("select r.escala, r.dataRegistro, c.culturaId, c.nome from Registro r "
+								+ "inner join r.cultura as c " + 
+								" where c.culturaId = " + culturasSelecionadas.get(i).getCulturaId());
 
-						@Override
-						public Object mapRow(ResultSet rs, int row) throws SQLException {
-							Registro registro = new Registro();
-							registro.setCultura(new Cultura());
-							registro.getCultura().setCulturaId(rs.getInt("culturaId"));
-							registro.setEscala(rs.getInt("escala"));
-							registro.getCultura().setNome(rs.getString("periodo_ingresso"));
-							registro.getCultura().setDataAtivacao(rs.getDate("dataRegistro"));
-							return registro;
-						}
-					});
-			registrosAll.add(registros);
+				@SuppressWarnings("unchecked")
+				List<Object> rs = query.list();
+				for (int a = 0; a < rs.size(); a++) {
+
+					Object[] obj = (Object[]) rs.get(a);
+
+					Registro registro = new Registro();
+					registro.setEscala(((Integer) obj[0]));
+					registro.setDataRegistro(((Date) obj[1]));
+
+					Cultura cultura = new Cultura();
+					cultura.setCulturaId((Integer) obj[2]);
+					cultura.setNome(((String) obj[3]));
+
+					registro.setCultura(cultura);
+
+					result.add(registro);
+
+				}
+				
+				if(!result.isEmpty())
+					registroList.add(result);
+				
+				result = new ArrayList<Registro>();
+
+			}
+
+			return registroList;
+
+		} catch (Exception e) {
+			throw e;
 
 		}
-		return registrosAll;
+
+		/*
+		 * List<Registro> registros = new ArrayList<>(); List<List<Registro>>
+		 * registrosAll = new ArrayList<>(); Session session =
+		 * HibernateUtil.getSessionFactory().openSession();
+		 * 
+		 * for (int i = 0; i < culturasSelecionadas.size(); i++) { String hql =
+		 * "select c.culturaId, r.escala, c.nome, r.dataRegistro from Registro r " +
+		 * " inner join r.cultura as c" + "	where c.culturaId =:id";
+		 * 
+		 * 
+		 * Query query = session.createQuery(hql); query.setParameter("id",
+		 * culturasSelecionadas.get(i).getCulturaId());
+		 * 
+		 * registros = query.list();
+		 * 
+		 * registrosAll.add(registros); }
+		 */
+
 	}
 }
