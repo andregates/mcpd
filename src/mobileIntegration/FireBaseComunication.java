e5c3af86ad2ded2a2340ddc6d0ff8d83b35debdf
joaoGabriel55 <j.quaresmasantos_98@hotmail.com>
2018-07-04 07:00:00 -0300

    Finals Fixs

--- src/mobileIntegration/FireBaseComunication.java
+++ src/mobileIntegration/FireBaseComunication.java
@@ -1,9 +1,13 @@
 package mobileIntegration;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
@@ -14,6 +18,7 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.tasks.Task;
 
 import models.Registro;
 
@@ -35,64 +40,58 @@ public class FireBaseComunication {
 
 	private ChildEventListener childEventRegistro;
 
-	private FireBaseComunication() throws IOException {
-
+	private FireBaseComunication() throws IOException  {
+		this.serviceAccount = new FileInputStream(
+				"C:\\Users\\Quaresma\\git\\mcpd\\target\\classes\\mcpd-b1333-firebase-adminsdk-4btsm-e3686817ae.json");
+		this.options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount))
+				.setDatabaseUrl("https://mcpd-b1333.firebaseio.com").build();
+		//this.childEventRegistro = childEventRegistro;
+	}
+	
+	public void save(Map<String, Registro> registros) {
 		System.out.println(getClass().getProtectionDomain().getCodeSource().getLocation()
 				+ "mcpd-b1333-firebase-adminsdk-4btsm-e3686817ae.json");
 
-		this.serviceAccount = new FileInputStream(
-				"C:\\Users\\Quaresma\\git\\mcpd\\target\\classes\\mcpd-b1333-firebase-adminsdk-4btsm-e3686817ae.json");
-
-		this.options = new FirebaseOptions.Builder().setCredentials(GoogleCredentials.fromStream(serviceAccount))
-				.setDatabaseUrl("https://mcpd-b1333.firebaseio.com").build();
-
 		FirebaseApp.initializeApp(options);
 
-		culturaReference = FirebaseDatabase.getInstance().getReference().child("Registro");
-
-		/*Registro registro = new Registro();
-
-		registro.setRegistroId(345); registro.setEscala(9);
-		registro.setObs("Massa hehe");
-		culturaReference.push().setValueAsync(registro);*/
-		 
-
-		  List<Registro> listaRegistros = new ArrayList<>();
-		  	
-		  childEventRegistro = culturaReference.child("dataRegistro").orderByChild("dataRegistro").addChildEventListener(new ChildEventListener() {
-	            @Override
-	            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-	                Registro registro =  dataSnapshot.getValue(Registro.class);
-	                registro.setKey(dataSnapshot.getKey());
-	                listaRegistros.add(registro);
-	                System.out.println(registro);
-	            }
-
-	            @Override
-	            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-	            }
-
-	            @Override
-	            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-	            }
-
-	            @Override
-	            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-	            }
-
-	            @Override
-	            public void onCancelled(DatabaseError databaseError) {
-
-	            }
-	        });
-		  culturaReference.addChildEventListener(childEventRegistro);
-		
-		System.out.println(culturaReference);
-
-		System.out.println("OK");
+		culturaReference = FirebaseDatabase.getInstance().getReference("/").child("Registro");
+				
+		culturaReference.setValueAsync(registros);
 	}
 
+		/*
+		 * 
+		 * 
+		 *  Task task =
+		 * culturaReference.push().setValue(registro);
+		 */
+		/*
+		 * List<Registro> listaRegistros = new ArrayList<>();
+		 * 
+		 * childEventRegistro =
+		 * culturaReference.child("dataRegistro").orderByChild("dataRegistro")
+		 * .addChildEventListener(new ChildEventListener() {
+		 * 
+		 * @Override public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+		 * Registro registro = dataSnapshot.getValue(Registro.class);
+		 * registro.setKey(dataSnapshot.getKey()); listaRegistros.add(registro);
+		 * 
+		 * }
+		 * 
+		 * @Override public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+		 * 
+		 * }
+		 * 
+		 * @Override public void onChildRemoved(DataSnapshot dataSnapshot) {
+		 * 
+		 * }
+		 * 
+		 * @Override public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+		 * 
+		 * }
+		 * 
+		 * @Override public void onCancelled(DatabaseError databaseError) {
+		 * 
+		 * } }); culturaReference.addChildEventListener(childEventRegistro);
+		 */
 }
--- src/util/FireBaseStart.java
+++ src/util/FireBaseStart.java
@@ -1,16 +1,27 @@
 package util;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import mobileIntegration.FireBaseComunication;
+import models.Registro;
 
 public class FireBaseStart {
 
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 
-		FireBaseComunication.getInstance();
-		
+		Map<String, Registro> registros = new HashMap<>();
+		Registro registro = new Registro();
+		registro.setRegistroId(87578);
+		registro.setEscala(955);
+		registro.setObs("Massa hehe EAE");
+		registros.put("rere", registro);
+
+		FireBaseComunication baseComunication = FireBaseComunication.getInstance();
+		baseComunication.save(registros);
+
 	}
 
 }
